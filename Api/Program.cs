1e353a595f963b1492aa05c11bfabf91d699dd8c
Jimit Ndiaye <jimitndiaye@gmail.com>
2024-12-04 16:53:49 +0000

    Correct typo in Aspire resource name for database

--- Api/Program.cs
+++ Api/Program.cs
@@ -6,7 +6,7 @@ builder.AddServiceDefaults();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.AddSqlServerClient("api-db");
+builder.AddSqlServerClient("db");
 var app = builder.Build();
 app.MapDefaultEndpoints();
 // Configure the HTTP request pipeline.
